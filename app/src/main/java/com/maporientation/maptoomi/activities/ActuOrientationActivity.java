d00915b1a41bb1a953041348ab72421914e51144
konami93 <konami_93@hotmail.fr>
2016-01-06 23:32:31 +0100

    Mon Profil

--- app/src/main/java/com/maporientation/maptoomi/activities/ActuOrientationActivity.java
+++ app/src/main/java/com/maporientation/maptoomi/activities/ActuOrientationActivity.java
@@ -1,15 +1,21 @@
 package com.maporientation.maptoomi.activities;
-
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import com.maporientation.maptoomi.fragments.facebookfragment;
+import com.maporientation.maptoomi.fragments.mapsfragment;
+import com.maporientation.maptoomi.fragments.twitterfragment;
+import com.maporientation.maptoomi.R;import java.lang.Override;import java.lang.SuppressWarnings;
 
-import com.maporientation.maptoomi.R;
-
-public class ActuOrientationActivity extends AppCompatActivity {
+public class ActuOrientationActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +24,74 @@ public class ActuOrientationActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        android.support.v4.app.FragmentManager fm = getSupportFragmentManager();
+        fm.beginTransaction().replace(R.id.content_frame,new facebookfragment()).commit();
     }
 
-}
+
+        @Override
+        public void onBackPressed()
+        {
+            DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+            if (drawer.isDrawerOpen(GravityCompat.START)) {
+                drawer.closeDrawer(GravityCompat.START);
+            } else {
+                super.onBackPressed();
+            }
+        }
+
+        @Override
+        public boolean onCreateOptionsMenu(Menu menu)
+        {
+            // Inflate the menu; this adds items to the action bar if it is present.
+            getMenuInflater().inflate(R.menu.menu_home, menu);
+            return true;
+        }
+
+        @Override
+        public boolean onOptionsItemSelected(MenuItem item)
+        {
+            // Handle action bar item clicks here. The action bar will
+            // automatically handle clicks on the Home/Up button, so long
+            // as you specify a parent activity in AndroidManifest.xml.
+            int id = item.getItemId();
+
+            //noinspection SimplifiableIfStatement
+            if (id == R.id.action_settings) {
+                Intent intention = new Intent(ActuOrientationActivity.this,mapsfragment.class);
+                startActivity(intention);
+                return true;
+            }
+
+            return super.onOptionsItemSelected(item);
+        }
+        @SuppressWarnings("StatementWithEmptyBody")
+        @Override
+        public boolean onNavigationItemSelected(MenuItem item)
+        {
+            android.support.v4.app.FragmentManager fm = getSupportFragmentManager();
+            int id = item.getItemId();
+
+            if (id == R.id.nav_facebook) {
+
+                fm.beginTransaction().replace(R.id.content_frame,new facebookfragment()).commit();
+
+            } else if (id == R.id.nav_twitter) {
+
+                fm.beginTransaction().replace(R.id.content_frame,new twitterfragment()).commit();
+
+            }
+            DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+            drawer.closeDrawer(GravityCompat.START);
+            return true;
+        }
+
+    }
+
+
